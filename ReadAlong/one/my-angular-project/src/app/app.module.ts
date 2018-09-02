2f6e6c828bf31fb3d139242b61dd37cf7b7f7de6
mocarskijosh <mocarskijosh@users.noreply.github.com>
2018-09-02 19:35:07 -0400

    clean up

--- ReadAlong/one/my-angular-project/src/app/app.module.ts
+++ ReadAlong/one/my-angular-project/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyButtonComponent } from './my-button/my-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyButtonComponent
   ],
   imports: [
     BrowserModule
